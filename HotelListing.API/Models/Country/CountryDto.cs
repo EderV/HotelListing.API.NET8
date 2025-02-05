45fd517d5b0a03f4a178a0c8b9cfc2a0eda6371d
eder-ohtic <eder.villar@ohtic.com>
2025-02-05 09:20:57 +0100

    REST API Development best practices. Automappings and Repositories

---
+++ HotelListing.API/Models/Country/CountryDto.cs
@@ -0,0 +1,13 @@
+﻿using HotelListing.API.Models.Hotels;
+
+namespace HotelListing.API.Models.Country
+{
+    public class CountryDto : BaseCountryDto
+    {
+
+        public string Id { get; set; }
+
+        public List<HotelDto> Hotels { get; set; }
+
+    }
+}
